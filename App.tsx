619bb18f5c7893f54264ec072905721c5134e47f
Guilherme Faustino Vargas <gfvargas2004@gmail.com>
2025-05-13 18:40:34 -0300

    Projeto parecido com o Lanterna Verde

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import PalmeirasOn from './assets/on.png';
+import PalmeirasOff from './assets/off.png';
+import { useState } from 'react';
 
 export default function App() {
+  const [isActive, setisActive] = useState(false);
+
+  function handleVerde (){
+    setisActive((oldValue:boolean) => {
+      return !oldValue
+    })
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      
+      <Text style = {styles.text} >Avanti Palestra!</Text>
+      <TouchableOpacity onPress={handleVerde}>
+      <Image source={isActive ? PalmeirasOff : PalmeirasOn}></Image>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOff: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  containerOn: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+    text: {
+    fontSize: 30,
+    color: 'green'
+  }
 });
+
+
+
